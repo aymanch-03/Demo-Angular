c21cbc445a34e9711b515c7b23b685b581e90ba2
aymanch-03 <aymanechakar03@gmail.com>
2025-08-12 14:34:13 +0100

    ui: angular routes basics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Products } from './products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'products',
+        component: Products
+        
+    }
+];

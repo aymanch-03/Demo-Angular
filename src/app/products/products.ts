8970d6d9b6789bc15baea0fb8b2b242ea2edbe55
aymanch-03 <aymanechakar03@gmail.com>
2025-08-12 15:14:59 +0100

    use ngOnInit & implement product delete

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -8,10 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './products.css',
   standalone: true
 })
-export class Products {
-  products = [
-    {id: 1, name: "Computer", price: 1000, selected: true},
-    {id: 2, name: "Printer", price: 1200, selected: false},
-    {id: 3, name: "Smart Phone", price: 980, selected: true},
-  ]
+export class Products implements OnInit {
+  products: Array<any> = [];
+
+  constructor(){
+  }
+
+  ngOnInit(): void {
+    this.products = [
+      {id: 1, name: "Computer", price: 1000, selected: true},
+      {id: 2, name: "Printer", price: 1200, selected: false},
+      {id: 3, name: "Smart Phone", price: 980, selected: true},
+    ]
+  }
+
+  handleDelete(id: number): void {
+    let c = confirm("Are you sure you want to delete this product?");
+    if(c){
+      this.products = this.products.filter(p => p.id !== id);
+    }
+  }
 }

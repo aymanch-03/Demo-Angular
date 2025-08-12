fb8b4be42c93e8ef5a4f2fb4cb151be74515b9c1
aymanch-03 <aymanechakar03@gmail.com>
2025-08-12 15:03:34 +0100

    ui: display products

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
   standalone: true
 })
 export class Products {
-
+  products = [
+    {id: 1, name: "Computer", price: 1000, selected: true},
+    {id: 2, name: "Printer", price: 1200, selected: false},
+    {id: 3, name: "Smart Phone", price: 980, selected: true},
+  ]
 }
